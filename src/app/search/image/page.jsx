4aaf8b055b55eca6adba4e557b41f7ebebbe01db
Harshal73 <harshal73gh@gmail.com>
2024-01-09 08:35:48 +0530

    image section completed

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import ImageSearchResults from "@/components/ImageSearchResults";
 
-export default function ImageSearchPage() {
-  return <div>ImageSearchPAge</div>;
+export default async function ImageSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+    &searchType=image`
+  );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+
+  console.log(data);
+  const results = data.items;
+  return <> {results && <ImageSearchResults results={data} />} </>;
 }
